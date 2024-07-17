07d26105f1742c53fa2647a06ea76fb09ed9dffd
door-main <doriamfloreszorrilla@gmail.com>
2024-07-17 03:09:34 -0500

    change tcp to nats client-gateway and products-ms,dockerfile

--- src/orders/orders.controller.ts
+++ src/orders/orders.controller.ts
@@ -1,41 +1,60 @@
-import { Controller, Get, Post, Body, Param, Inject, ParseUUIDPipe, Query, Patch } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Param,
+  Inject,
+  ParseUUIDPipe,
+  Query,
+  Patch,
+} from '@nestjs/common';
 import { ClientProxy, RpcException } from '@nestjs/microservices';
-import { ORDER_SERVICE } from 'src/config';
+import { NATS_SERVICE } from 'src/config';
 import { CreateOrderDto, OrderPaginationDto, StatusDto } from './dto';
-import { catchError } from 'rxjs';
+import { catchError, firstValueFrom } from 'rxjs';
 
 @Controller('orders')
 export class OrdersController {
-  constructor(
-    @Inject(ORDER_SERVICE) private readonly ordersClient: ClientProxy
-  ) { }
+  constructor(@Inject(NATS_SERVICE) private readonly client: ClientProxy) {}
 
   @Post()
   create(@Body() createOrderDto: CreateOrderDto) {
-    return this.ordersClient.send('createOrder', createOrderDto)
+    return this.client.send('createOrder', createOrderDto);
   }
 
   @Get()
-  findAll(@Query() orderPaginationDto: OrderPaginationDto) {
-    return this.ordersClient.send('findAllOrders', orderPaginationDto)
+  async findAll(@Query() orderPaginationDto: OrderPaginationDto) {
+    try {
+      const orders = await firstValueFrom(
+        this.client.send('findAllOrders', orderPaginationDto),
+      );
+      return orders;
+    } catch (err) {
+      throw new RpcException(err);
+    }
   }
 
   @Get(':id')
   findOne(@Param('id', ParseUUIDPipe) id: string) {
-    return this.ordersClient.send('findOneOrder', { id }).pipe(
-      catchError(err => { throw new RpcException(err) })
-    )
+    return this.client.send('findOneOrder', { id }).pipe(
+      catchError((err) => {
+        throw new RpcException(err);
+      }),
+    );
   }
 
   @Patch(':id')
   changeOrderStatus(
     @Param('id', ParseUUIDPipe) id: string,
-    @Body() statusDto: StatusDto
+    @Body() statusDto: StatusDto,
   ) {
-    return this.ordersClient.send('ChangeOrderStatus', { id, status: statusDto.status })
+    return this.client
+      .send('ChangeOrderStatus', { id, status: statusDto.status })
       .pipe(
-        catchError(err => { throw new RpcException(err) })
-      )
+        catchError((err) => {
+          throw new RpcException(err);
+        }),
+      );
   }
-
 }
--- src/products/products.module.ts
+++ src/products/products.module.ts
@@ -1,21 +1,9 @@
 import { Module } from '@nestjs/common';
 import { ProductsController } from './products.controller';
-import { ClientsModule, Transport } from '@nestjs/microservices';
-import { NATS_SERVICE, envs } from 'src/config';
+import { NatsModule } from 'src/transports/nats.module';
 
 @Module({
   controllers: [ProductsController],
-  providers: [],
-  imports: [
-    ClientsModule.register([
-      {
-        name: NATS_SERVICE,
-        transport: Transport.NATS,
-        options: {
-          servers: envs.natsServers
-        },
-      },
-    ]),
-  ],
+  imports: [NatsModule],
 })
-export class ProductsModule { }
+export class ProductsModule {}
