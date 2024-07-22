3c490cd16784c4fe61c6cbda7c137fea0fd6cdeb
door-main <doriamfloreszorrilla@gmail.com>
2024-07-22 03:09:02 -0500

    connection to auth-ms with decorators

---
+++ src/auth/auth.controller.ts
@@ -0,0 +1,38 @@
+import { Body, Controller, Inject, Post, UseGuards } from '@nestjs/common';
+import { ClientProxy, RpcException } from '@nestjs/microservices';
+import { NATS_SERVICE } from 'src/config';
+import { LoginUserDto, RegisterUserDto } from './dto';
+import { catchError } from 'rxjs';
+import { AuthGuard } from './guards/auth.guard';
+import { Token, User } from './decorators';
+import { CurrentUser } from './interfaces/current-user.interface';
+
+@Controller('auth')
+export class AuthController {
+  constructor(@Inject(NATS_SERVICE) private readonly client: ClientProxy) {}
+
+  @Post('register')
+  registerUser(@Body() registerUserDto: RegisterUserDto) {
+    return this.client.send('auth.register.user', registerUserDto).pipe(
+      catchError((err) => {
+        throw new RpcException(err);
+      }),
+    );
+  }
+
+  @Post('login')
+  loginUser(@Body() loginUserDto: LoginUserDto) {
+    return this.client.send('auth.login.user', loginUserDto).pipe(
+      catchError((err) => {
+        throw new RpcException(err);
+      }),
+    );
+  }
+
+  @UseGuards(AuthGuard)
+  @Post('verify')
+  veriryToken(@User() user: CurrentUser, @Token() token: string) {
+    return { user, token };
+    // return this.client.send('auth.verify.user', {});
+  }
+}
